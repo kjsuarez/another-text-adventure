bab502a08542b1791edb234614ca4b05bd20e7a9
kjsuarez <kjsuarez94@gmail.com>
2018-07-04 23:43:12 -0700

    added header links

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header.component";
 import { GameComponent } from "./game.component";
 import { RoomComponent } from "./room.component";
 import { ChoiceComponent } from "./choice.component";
@@ -15,6 +16,7 @@ import { routing } from "./app.routing";
 @NgModule({
     declarations: [
       AppComponent,
+      HeaderComponent,
       GameComponent,
       RoomComponent,
       ChoiceComponent,
---
+++ assets/app/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Game } from './game.model';
+import { GameService } from './game.service';
+import { Room } from './room.model';
+import { Choice } from './choice.model';
+
+@Component({
+  selector: 'header',
+  templateUrl: './header.component.html'
+})
+
+export class HeaderComponent{
+
+}
