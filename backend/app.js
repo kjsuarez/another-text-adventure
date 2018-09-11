3d41ad7fd6fa3fa05df1628de37df7da68d0ebde
kjsuarez <kjsuarez94@gmail.com>
2018-09-10 18:13:03 -0700

    successful request to node server from browser

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.use(function (req, res, next) {
-  res.send("greetings mortal");
+app.use('/api/games', function (req, res, next) {
+  const games = [
+    { id: "0", name: "another text adventure", start_room_id: "0", current_room_id: null },
+    { id: "1", name: "Dungeonland", start_room_id: "3", current_room_id: null },
+    { id: "2", name: "roomless game", start_room_id: null, current_room_id: null }
+  ]
+
+  res.status(200).json({
+    message: "sending games",
+    obj: games
+  });
 });
 
 module.exports = app;
