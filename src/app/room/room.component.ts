b254fef1ae5d3a969e5cde76651bda012629deae
kjsuarez <kjsuarez94@gmail.com>
2018-10-05 10:29:10 -0700

    frontend updates

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -5,7 +5,8 @@ import { Room } from './room.model';
 
 @Component({
   selector: 'room',
-  templateUrl: './room.component.html'
+  templateUrl: './room.component.html',
+  styleUrls: ['./room.component.css']
 })
 
 export class RoomComponent {
